a648b550fe607f79e10aea4fc9157c8ace0f7db8
Dominic <dominic.pr93@gmail.com>
2019-11-17 17:35:26 +0100

    updated text in the html and made the yearly numbers update automatically

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,22 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
+  // calculate dates
+  let currentYear = new Date().getFullYear();
+  let currentAge = currentYear - new Date('August 31, 1993').getFullYear();
+  document.querySelector('#current-year').textContent = currentYear;
+  document.querySelector('#current-age').textContent = currentAge;
+
   //special text
-  if(window.location.hash === "#hire-me-redox"){
+  if (window.location.hash === "#hire-me-redox") {
     document.querySelector(".hash-text").innerText = " Redox";
   }
 
-  //typewriter
+  // typewriter
   var typewriterElement = document.querySelector('.typewriter');
 
   var typewriter = new Typewriter(typewriterElement, {
-      loop: true
+    loop: true
   });
 
   typewriter.typeString('Web Developer.')
@@ -27,46 +33,46 @@ document.addEventListener('DOMContentLoaded', (event) => {
     .pauseFor(2500)
     .start();
 
-    //add scroll jumps
-    function scrollToTargetAdjusted(x){
-      const element = document.querySelector(x);
-      const offset = document.querySelector("header").offsetHeight; //height of header in px
-      const bodyRect = document.body.getBoundingClientRect().top;
-      const elementRect = element.getBoundingClientRect().top;
-      const elementPosition = elementRect - bodyRect;
-      const offsetPosition = elementPosition - offset;
+  // add scroll jumps
+  function scrollToTargetAdjusted(x) {
+    const element = document.querySelector(x);
+    const offset = document.querySelector("header").offsetHeight; //height of header in px
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = element.getBoundingClientRect().top;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - offset;
 
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: 'smooth'
-      });
-    }
-
-    document.querySelector('.scroll-down').addEventListener("click",function(){
-      scrollToTargetAdjusted('main');
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth'
     });
-    document.querySelector('.hero button').addEventListener("click",function(){
-      scrollToTargetAdjusted('main');
-    });
+  }
 
-    document.querySelectorAll('.header .header__menu li').forEach(li => {
-      li.addEventListener("click",function(){
-        scrollToTargetAdjusted(li.getAttribute("data-jump-to"));
-      });
-    });
+  document.querySelector('.scroll-down').addEventListener("click", function() {
+    scrollToTargetAdjusted('main');
+  });
+  document.querySelector('.hero button').addEventListener("click", function() {
+    scrollToTargetAdjusted('main');
+  });
 
-  //scroll spy for header menu
+  document.querySelectorAll('.header .header__menu li').forEach(li => {
+    li.addEventListener("click", function() {
+      scrollToTargetAdjusted(li.getAttribute("data-jump-to"));
+    });
+  });
+
+  // scroll spy for header menu
   let last_known_scroll_position = 0;
   let ticking = false;
   let scrolledDown = false;
 
   function doSomething(scroll_pos) { // Do something with the scroll position
     let elementHeight = document.querySelector(".hero").offsetHeight - document.querySelector("header").offsetHeight;
-    if(scroll_pos > elementHeight && !scrolledDown){
+    if (scroll_pos > elementHeight && !scrolledDown) {
       scrolledDown = true;
       document.querySelector("header").classList.add("header--scrolled-down");
     }
-    if(scroll_pos <= elementHeight && scrolledDown){
+    if (scroll_pos <= elementHeight && scrolledDown) {
       scrolledDown = false;
       document.querySelector("header").classList.remove("header--scrolled-down");
     }
@@ -82,7 +88,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
       });
 
       ticking = true;
-      }
-    });
+    }
+  });
 
 });
