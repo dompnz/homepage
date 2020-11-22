5781a2daa71ebbe6e096b537bf83726a4f658535
Dominic <dominic.pr93@gmail.com>
2020-11-22 14:06:33 +0100

    mostly a content update

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,96 @@
 /**/
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
+	// calculate dates
+	let currentDate = new Date();
+	let birthdayDate = new Date("August 31, 1993");
+	let diffTimeMiliseconds = Math.abs(currentDate - birthdayDate);
+	let diffTimeYears = diffTimeMiliseconds / (1000 * 60 * 60 * 24 * 365);
+	let currentAge = Math.floor(diffTimeYears);
+	let currentYear = currentDate.getFullYear();
+	document.querySelector("#current-age").textContent = currentAge;
+	document.querySelector("#current-year").textContent = currentYear;
 
-  // calculate dates
-  let currentDate = new Date();
-  let birthdayDate = new Date('August 31, 1993');
-  let diffTimeMiliseconds = Math.abs(currentDate - birthdayDate);
-  let diffTimeYears = diffTimeMiliseconds / (1000 * 60 * 60 * 24 * 365);
-  let currentAge = Math.floor(diffTimeYears);
-  let currentYear = currentDate.getFullYear();
-  document.querySelector('#current-age').textContent = currentAge;
-  document.querySelector('#current-year').textContent = currentYear;
+	//special text
+	if (window.location.hash === "#loop") {
+		document.querySelector(".hash-text").innerText = ` ${window.location.hash.substring(1)}`;
+	}
 
-  //special text
-  if (window.location.hash === "#hire-me-redox") {
-    document.querySelector(".hash-text").innerText = " Redox";
-  }
+	// typewriter
+	var typewriterElement = document.querySelector(".typewriter");
 
-  // typewriter
-  var typewriterElement = document.querySelector('.typewriter');
+	var typewriter = new Typewriter(typewriterElement, {
+		loop: true,
+	});
 
-  var typewriter = new Typewriter(typewriterElement, {
-    loop: true
-  });
+	typewriter
+		.typeString("Web Developer.")
+		.pauseFor(2500)
+		.deleteChars(10)
+		.typeString("<span>Enthusiast.</span>")
+		.pauseFor(2500)
+		.deleteAll()
+		.typeString("Cat Lover. 🐱")
+		.pauseFor(2500)
+		.pauseFor(2500)
+		.start();
 
-  typewriter.typeString('Web Developer.')
-    .pauseFor(2500)
-    .deleteChars(10)
-    .typeString('<span>Enthusiast.</span>')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('Gamer.')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('Cat Lover.')
-    .pauseFor(2500)
-    .start();
+	// add scroll jumps
+	function scrollToTargetAdjusted(x) {
+		const element = document.querySelector(x);
+		const offset = document.querySelector("header").offsetHeight; //height of header in px
+		const bodyRect = document.body.getBoundingClientRect().top;
+		const elementRect = element.getBoundingClientRect().top;
+		const elementPosition = elementRect - bodyRect;
+		const offsetPosition = elementPosition - offset;
 
-  // add scroll jumps
-  function scrollToTargetAdjusted(x) {
-    const element = document.querySelector(x);
-    const offset = document.querySelector("header").offsetHeight; //height of header in px
-    const bodyRect = document.body.getBoundingClientRect().top;
-    const elementRect = element.getBoundingClientRect().top;
-    const elementPosition = elementRect - bodyRect;
-    const offsetPosition = elementPosition - offset;
+		window.scrollTo({
+			top: offsetPosition,
+			behavior: "smooth",
+		});
+	}
 
-    window.scrollTo({
-      top: offsetPosition,
-      behavior: 'smooth'
-    });
-  }
+	document.querySelector(".scroll-down").addEventListener("click", function () {
+		scrollToTargetAdjusted("main");
+	});
+	document.querySelector(".hero button").addEventListener("click", function () {
+		scrollToTargetAdjusted("main");
+	});
 
-  document.querySelector('.scroll-down').addEventListener("click", function () {
-    scrollToTargetAdjusted('main');
-  });
-  document.querySelector('.hero button').addEventListener("click", function () {
-    scrollToTargetAdjusted('main');
-  });
+	document.querySelectorAll(".header .header__menu li").forEach((li) => {
+		li.addEventListener("click", function () {
+			scrollToTargetAdjusted(li.getAttribute("data-jump-to"));
+		});
+	});
 
-  document.querySelectorAll('.header .header__menu li').forEach(li => {
-    li.addEventListener("click", function () {
-      scrollToTargetAdjusted(li.getAttribute("data-jump-to"));
-    });
-  });
+	// scroll spy for header menu
+	let last_known_scroll_position = 0;
+	let ticking = false;
+	let scrolledDown = false;
 
-  // scroll spy for header menu
-  let last_known_scroll_position = 0;
-  let ticking = false;
-  let scrolledDown = false;
+	function doSomething(scroll_pos) {
+		// Do something with the scroll position
+		let elementHeight = document.querySelector(".hero").offsetHeight - document.querySelector("header").offsetHeight;
+		if (scroll_pos > elementHeight && !scrolledDown) {
+			scrolledDown = true;
+			document.querySelector("header").classList.add("header--scrolled-down");
+		}
+		if (scroll_pos <= elementHeight && scrolledDown) {
+			scrolledDown = false;
+			document.querySelector("header").classList.remove("header--scrolled-down");
+		}
+	}
 
-  function doSomething(scroll_pos) { // Do something with the scroll position
-    let elementHeight = document.querySelector(".hero").offsetHeight - document.querySelector("header").offsetHeight;
-    if (scroll_pos > elementHeight && !scrolledDown) {
-      scrolledDown = true;
-      document.querySelector("header").classList.add("header--scrolled-down");
-    }
-    if (scroll_pos <= elementHeight && scrolledDown) {
-      scrolledDown = false;
-      document.querySelector("header").classList.remove("header--scrolled-down");
-    }
-  }
+	window.addEventListener("scroll", function (e) {
+		last_known_scroll_position = window.scrollY;
 
-  window.addEventListener('scroll', function (e) {
-    last_known_scroll_position = window.scrollY;
+		if (!ticking) {
+			window.requestAnimationFrame(function () {
+				doSomething(last_known_scroll_position);
+				ticking = false;
+			});
 
-    if (!ticking) {
-      window.requestAnimationFrame(function () {
-        doSomething(last_known_scroll_position);
-        ticking = false;
-      });
-
-      ticking = true;
-    }
-  });
-
+			ticking = true;
+		}
+	});
 });
